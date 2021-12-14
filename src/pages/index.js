029db772dd646c0970ea31c14ad7d10bc6f84b23
Chandika Hettiarachchi <chandikalakshan@gmail.com>
2021-12-14 19:42:04 +0530

    Added image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
 	return (
 		<Layout pageTitle="Home Page">
 			<p>This is the Home Page.</p>
+			<StaticImage
+				alt="Window 11 user"
+				src="../images/laptop.jpeg"
+			></StaticImage>
 		</Layout>
 	);
 };
